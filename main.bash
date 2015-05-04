28a2db66c89681e1448256817b80cdaa436c348c
John Mellor <mellor@gmail.com>
2015-05-04 18:19:02 +0100

    Add git-prompt.sh and git-completion.bash
    from https://github.com/git/git/tree/6b1258b07b8ad5671f36bf7e998e905a0142c223/contrib/completion

--- main.bash
+++ main.bash
@@ -7,6 +7,30 @@ __source_relative() {
 }
 
 
+# PROMPT
+
+__source_relative git-prompt.sh
+
+# Show if there are unstaged (*) and/or staged (+) changes
+export GIT_PS1_SHOWDIRTYSTATE=1
+
+# Show if there is anything stashed ($)
+#export GIT_PS1_SHOWSTASHSTATE=1
+
+# Show if there are untracked files (%)
+export GIT_PS1_SHOWUNTRACKEDFILES=1
+
+# Show how we're tracking relative to upstream
+export GIT_PS1_SHOWUPSTREAM="verbose"
+
+export PS1="\[\e[33m$(tput bold)\]\w\[\e[0m\]$(__git_ps1 ' (%s)')$ "
+
+
+# GIT
+
+__source_relative git-completion.bash
+
+
 # MISC
 
 alias l='ls -hF --color=auto'
